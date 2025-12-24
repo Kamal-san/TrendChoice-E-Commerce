a7f5bbf6a048d82f37709d9407f928cf7a9bc047
Kamaleshwaran <kamaleshw046@gmail.com>
2025-12-24 15:22:50 +0530

    Refactor CartItemDetails to use environment variable

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-import BASE_URL from "../config/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 import { useState } from "react";
 
 import { formatMoney } from "../../utils/money";
@@ -74,4 +75,5 @@ export function CartItemDetails({ cartItem, loadCart }) {
   );
 
 }
+
 
